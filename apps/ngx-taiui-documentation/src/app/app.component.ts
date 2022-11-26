cf3aba40b1f8625b948242beb8590cf3def80598
Raffaele Sarracino <contacts@raffaelesarracino.it>
2022-11-26 20:26:10 +0100

    Initial commit

---
+++ apps/ngx-taiui-documentation/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-taiui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngx-taiui-documentation';
+}
